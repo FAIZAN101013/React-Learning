b1c975b27a0c2f9943f52975a73fd8fda409c4ad
Mohammed Faizan <your.email@example.com>
2025-05-28 22:56:40 +0530

    Dinamic input

--- 3/01-starting-project/src/components/Header.jsx
+++ 3/01-starting-project/src/components/Header.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
+const reactDescription = ['Fundamental', 'Core', 'Crucial', 'Essential', 'Key']
+
+
+const genRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 const Header = () => {
+  const randomDescription = reactDescription[genRandomInt(4)]
+
   return (
     <div>
-          <header>
-              <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-              <h1>React Essentials</h1>
-              <p>
-                  Fundamental React concepts you will need for almost any app you are
-                  going to build!
-              </p>
-          </header>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {randomDescription} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
     </div>
   )
 }
